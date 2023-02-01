7fca2617ea768d50c9cbcc283a552fe4dc408e01
Adeesh M Shrestha <35621337+adeeshmans@users.noreply.github.com>
2023-02-01 21:37:15 +0000

    modified the logic

--- main.py
+++ main.py
@@ -19,11 +19,10 @@ if height >= 120:
     bill = 12
     print("Adult tickets are $12.")
   
-    # Get input if user wants to add a photo to the ticket and add $3v regardless of age
-  wants_photo = input("Do you want a photo taken? Y or N. ")
+    # Get input if user wants to add a photo to the ticket and add $3v regardless of age.
+ wants_photo = input("Do you want a photo taken? Y or N. ")
   if wants_photo == "Y":
     bill += 3
-  
   print(f"Your final bill is ${bill}")
 
 else:
